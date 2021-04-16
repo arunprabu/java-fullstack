03252da2d84b1bd619c94a29bd2730831bc6a399
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2021-04-16 11:59:40 +0530

    product service changes

--- labs/examples/shopping-cart-product-service/src/main/java/com/examples/scart/product/ProductServiceMain.java
+++ labs/examples/shopping-cart-product-service/src/main/java/com/examples/scart/product/ProductServiceMain.java
@@ -4,12 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @RestController
 public class ProductServiceMain {
 
